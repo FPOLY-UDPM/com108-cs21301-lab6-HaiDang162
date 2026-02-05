112ecba93b1bb24decacc7a1ba66353a93e02fca
Hai Dang <haidang@example.com>
2026-02-05 08:40:19 +0700

    Dang nop bai

--- bai2.c
+++ bai2.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Lã Hải Đăng ]
+ * MSSV:      [PS47692]
+ * Lớp:       [CS21301]
  *****************************************************************************/
 
 //  BÀI 2: TÌM GIÁ TRỊ LỚN NHẤT VÀ NHỎ NHẤT TRONG MẢNG 
@@ -9,4 +9,31 @@
 //  Output: Xuất ra màn hình giá trị nhỏ nhất và lớn nhất của mảng
 
 // VIẾT CODE Ở ĐÂY
-
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Nhap n: ");
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        printf("Nhap arr[%d]: ", i);
+        scanf("%d", &arr[i]);
+    }
+    int max = arr[0];
+    int min = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    printf("Gia tri lon nhat trong mang la: %d\n", max);
+    printf("Gia tri nho nhat trong mang la: %d\n", min);
+    return 0;
+}
